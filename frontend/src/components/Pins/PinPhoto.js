66d7126974965ffbbd4fe53d8c67fd2d1c65744b
ningxiao <dsgrichard@hotmail.com>
2023-04-26 09:21:21 -0700

    masonry

--- frontend/src/components/Pins/PinPhoto.js
+++ frontend/src/components/Pins/PinPhoto.js
@@ -1,9 +1,21 @@
-
+import './PinPhoto.css'
 
 const PinPhotoContainer = ({pin}) => {
     return(
-        <div className="pin-photo">
-            <img src={pin?.imageUrl}></img>
+        <div className="pin-photo-container">
+            <div className="pin-photo">
+                <img src={pin?.imageUrl}></img>
+                <div className={`pin-item-hover`}>
+                    <div className={`pin-item-hover-board-name`}>
+                        <div className={`pin-dropdown-trigger`} >
+                            <h1>board</h1>
+                            <i className='fa-solid fa-chevron-down fa-xs'></i>
+                        </div>
+                        <button className="save-button">Save</button>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     )
 
